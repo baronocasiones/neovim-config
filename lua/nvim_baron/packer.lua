4f9d60285a37213607c5a6b107d3bd15e00cd846
Baron <baronocasiones@gmail.com>
2025-09-04 04:00:50 +0800

    Added File tree

--- lua/nvim_baron/packer.lua
+++ lua/nvim_baron/packer.lua
@@ -35,4 +35,21 @@ return require('packer').startup(function(use)
       end
   }
 
+  use {
+      'nvim-telescope/telescope-file-browser.nvim',
+      requires = { 'nvim-telescope/telescope.nvim' },
+      config = function()
+          require("telescope").load_extension "file_browser"
+      end
+  }
+  use {
+      'nvim-tree/nvim-tree.lua',
+      requires = {
+          'nvim-tree/nvim-web-devicons', -- optional, for file icons
+      },
+      config = function()
+          require("nvim-tree").setup()
+      end
+  }
+
 end)
