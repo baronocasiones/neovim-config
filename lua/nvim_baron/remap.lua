3d5ff7ea7e7f1a2502a88721ef97918836c7f1b3
Baron <baronocasiones@gmail.com>
2025-09-07 04:29:58 +0800

    update

--- lua/nvim_baron/remap.lua
+++ lua/nvim_baron/remap.lua
@@ -8,18 +8,22 @@ vim.keymap.set('n', '<C-u>', '<C-u>zz')
 vim.keymap.set('n', 'n', 'nzzzv')
 vim.keymap.set('n', 'N', 'nzzzv')
 
-vim.keymap.set('x', '<leader>p', '"_dP')
-
 vim.api.nvim_create_autocmd('VimEnter', {
     callback = function() 
         vim.keymap.set('n', '<leader>y', "\"+y")
         vim.keymap.set('v', '<leader>y', "\"+y")
         vim.keymap.set('n', '<leader>Y', "\"+Y")
+        vim.keymap.set('n', '<leader>wh', '<C-w>h')
+        vim.keymap.set('n', '<leader>wl', '<C-w>l')
+        vim.keymap.set('n', '<leader>wj', '<C-w>j')
+        vim.keymap.set('n', '<leader>wk', '<C-w>k')
+        vim.keymap.set('x', '<leader>p', '"_dP')
+        vim.keymap.set('v', '<leader>p', '"_dP')
+        vim.keymap.set('n', '<leader>d', "\"_d")
+        vim.keymap.set('v', '<leader>d', "\"_d")
     end
 })
 
-vim.keymap.set('n', '<leader>d', "\"_d")
-vim.keymap.set('v', '<leader>d', "\"_d")
 
 vim.keymap.set('n', '<C-f>', '<cmd>silent !mux neww tmux-sessionizer<CR>')
 vim.keymap.set('n', '<leader>f', function()
@@ -29,11 +33,4 @@ end)
 vim.keymap.set('n', '<leader>s', ':%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>')
 vim.keymap.set('n', '<leader>x', '<cmd>!chmod %<CR>', { silent = true })
 
-vim.api.nvim_create_autocmd('VimEnter', {
-    callback = function() 
-        vim.keymap.set('n', '<leader>wh', '<C-w>h')
-        vim.keymap.set('n', '<leader>wl', '<C-w>l')
-        vim.keymap.set('n', '<leader>wj', '<C-w>j')
-        vim.keymap.set('n', '<leader>wk', '<C-w>k')
-    end
-})
+vim.keymap.set('n', '<leader>st', '<cmd>NvimTreeToggle<CR>')
